5f64f89f87228eba3515a89527f77f3cadb8590e
benjaminGUYT <55363637+benjaminGUYT@users.noreply.github.com>
2020-03-15 18:30:35 +0100

    Rendu final

--- src/client/Application.java
+++ src/client/Application.java
@@ -17,41 +17,54 @@ class Application {
 		private final static String HOST = "localhost";
 	
 	public static void main(String[] args) {
-		Socket s = null;		
-		try {
-			/* On créé la socket allant questionner le serveur */
-			s = new Socket(HOST, PORT_SERVICE);
+		while(true) {
+			Socket s = null;		
+			try {
+				/* On créé la socket allant questionner le serveur */
+				s = new Socket(HOST, PORT_SERVICE);
 
-			BufferedReader sin = new BufferedReader (new InputStreamReader(s.getInputStream ( )));
-			PrintWriter sout = new PrintWriter (s.getOutputStream ( ), true);
-			BufferedReader clavier = new BufferedReader(new InputStreamReader(System.in));			
-			
-			System.out.println("Connecté au serveur " + s.getInetAddress() + ":"+ s.getPort());
-			
-			String line;
-			/* Menu et choix du service */
-			line = sin.readLine();
-			System.out.println(line.replaceAll("##", "\n"));
-			/* Saisie/envoie du choix */
-			sout.println(clavier.readLine());
-			
-			/* Réception/affichage de la question */
-			System.out.println(sin.readLine());
-			/* Saisie clavier/envoie au service de la réponse */
-			sout.println(clavier.readLine());
-			/* Réception/affichage de la réponse */
-			System.out.println(sin.readLine());
+				BufferedReader sin = new BufferedReader (new InputStreamReader(s.getInputStream ( )));
+				PrintWriter sout = new PrintWriter (s.getOutputStream ( ), true);
+				BufferedReader clavier = new BufferedReader(new InputStreamReader(System.in));			
+				
+				System.out.println("\n\nConnecté au serveur " + s.getInetAddress() + ":"+ s.getPort());
+				
+				String line;
+
+				line = sin.readLine();
+				
+				System.out.println(line.replaceAll("##", "\n"));
+				sout.println(clavier.readLine());
+
+				while(true) {
+					try {
+						line = sin.readLine();
+						if(line.equals("succes") || line.equals("fail")) {
+							if(!line.equals("succes"))
+								System.out.println(line.replaceAll("##", "\n"));
+							break;
+						}
+						if(line.charAt(0) == '&' && line.charAt(1) == '&') {
+							System.out.println(line.replaceAll("##", "\n").replaceAll("&&", ""));
+						}
+						else {
+							System.out.println(line.replaceAll("##", "\n"));
+							sout.println(clavier.readLine());
+						}
+					}catch (Exception e) {
+					System.out.println("Problème : Vérifiez vos entrées, assurez-vous que votre serveur ftp est ouvert pour certains services !");
+					break;
+				}
+				}}
 				
 			
-		}
-		catch (IOException e) { 
-			System.err.println("Fin de la connexion"); 
-		}
-
-		/* Refermer dans tous les cas la socket */
-		try { 
-			if (s != null) s.close(); 
-		} catch (IOException e2) {
-		}		
+			catch (IOException e) { 
+				System.err.println("Fin de la connexion"); 
+			}
+			/* Refermer dans tous les cas la socket */
+			try { 
+				if (s != null) s.close(); 
+			} catch (IOException e2) {}		
+			}
 	}
 }
